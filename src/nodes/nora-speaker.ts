116ad2562abd819d76abfe673066d25fd24490d7
Andrei Tatar <tatarandrei@gmail.com>
2019-02-01 17:14:11 +0200

    add speaker node

---
+++ src/nodes/nora-speaker.ts
@@ -0,0 +1,100 @@
+import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
+import { publishReplay, refCount, skip, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { NodeInterface } from '../node';
+import { NoraService } from '../nora';
+
+interface SpeakerState {
+    on: boolean;
+    online: boolean;
+    currentVolume: number;
+    isMuted: boolean;
+}
+
+module.exports = function (RED) {
+    RED.nodes.registerType('nora-speaker', function (this: NodeInterface, config) {
+        RED.nodes.createNode(this, config);
+
+        const noraConfig = RED.nodes.getNode(config.nora);
+        if (!noraConfig || !noraConfig.token) { return; }
+
+        const close$ = new Subject();
+        const state$ = new BehaviorSubject<SpeakerState>({
+            on: true,
+            online: true,
+            currentVolume: 30,
+            isMuted: false,
+        });
+        const stateString$ = new Subject<string>();
+
+        const device$ = NoraService
+            .getService(RED)
+            .getConnection(noraConfig, this, stateString$)
+            .pipe(
+                switchMap(connection => connection.addDevice(config.id, {
+                    type: 'speaker',
+                    name: config.devicename,
+                    roomHint: config.roomhint || undefined,
+                    state: state$.value,
+                })),
+                publishReplay(1),
+                refCount(),
+                takeUntil(close$),
+            );
+
+        device$.pipe(
+            switchMap(d => d.errors$),
+            takeUntil(close$),
+        ).subscribe(err => this.warn(err));
+
+        combineLatest(device$, state$)
+            .pipe(
+                tap(([_, state]) => notifyState(state)),
+                skip(1),
+                takeUntil(close$),
+            )
+            .subscribe(([device, on]) => device.updateState({ on }));
+
+        device$.pipe(
+            switchMap(d => d.state$),
+            takeUntil(close$),
+        ).subscribe((state: SpeakerState) => {
+            notifyState(state);
+            this.send({
+                payload: {
+                    on: state.on,
+                    volume: state.currentVolume,
+                },
+                topic: config.topic
+            });
+        });
+
+        this.on('input', msg => {
+            const update: Partial<SpeakerState> = {};
+            if (typeof msg === 'object' && typeof msg.payload === 'object') {
+                const payload = msg.payload;
+                if ('on' in payload) {
+                    update.on = !!payload.on;
+                }
+                if ('volume' in payload && typeof payload.volume === 'number' && isFinite(payload.volume)) {
+                    update.currentVolume = Math.floor(Math.max(0, Math.min(100, payload.volume)));
+                }
+                if (Object.keys(update).length > 0) {
+                    state$.next({
+                        ...state$.value,
+                        ...update,
+                    });
+                }
+            }
+        });
+
+        this.on('close', () => {
+            close$.next();
+            close$.complete();
+        });
+
+        function notifyState(state: SpeakerState) {
+            stateString$.next(`(${state.on ? 'on' : 'off'}:${state.currentVolume})`);
+        }
+    });
+};
+
