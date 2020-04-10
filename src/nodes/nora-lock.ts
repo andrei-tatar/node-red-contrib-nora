26b94bb34bb11d5f2f9fb7452f44aac844fdfce4
Jiri Vondrus <jvondrus@gmail.com>
2020-04-10 15:19:39 +0200

    Two-factor authentication

--- src/nodes/nora-lock.ts
+++ src/nodes/nora-lock.ts
@@ -44,6 +44,8 @@ module.exports = function (RED) {
                     name: config.devicename,
                     roomHint: config.roomhint || undefined,
                     state: state$.value,
+                    twoFactor: config.twoFactor ? config.twoFactorMode : undefined,
+                    pin: ( config.twoFactor && config.twoFactorMode == 'pin') ? (config.twoFactorPin+'') : undefined
                 })),
                 publishReplay(1),
                 refCount(),
